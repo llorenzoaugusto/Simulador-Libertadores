8862e3d9219e58972bce6f08802ee07e71319f1a
llorenzoaugusto <114509802+llorenzoaugusto@users.noreply.github.com>
2025-06-03 03:54:27 +0000

    fix build

--- src/constants.ts
+++ src/constants.ts
@@ -2,22 +2,22 @@ import { Team } from './types';
 
 export const INITIAL_TEAMS: Team[] = [
   // Left side teams
-  { id: 'sao_paulo', name: 'São Paulo', apiId: null, logo: '/logos/337.png' },
-  { id: 'atletico_nacional', name: 'Atlético Nacional', apiId: null, logo: '/logos/427.png' },
-  { id: 'ldu_quito', name: 'LDU Quito', apiId: null, logo: '/logos/5270338.png' },
-  { id: 'botafogo', name: 'Botafogo', apiId: null, logo: '/logos/316.png' },
-  { id: 'river_plate', name: 'River Plate', apiId: null, logo: '/logos/94.png' },
-  { id: 'libertad', name: 'Libertad', apiId: null, logo: '/logos/5280161.png' },
-  { id: 'palmeiras', name: 'Palmeiras', apiId: null, logo: '/logos/329.png' },
-  { id: 'universitario', name: 'Universitario', apiId: null, logo: '/logos/1450.png' },
+  { id: 'sao_paulo', name: 'São Paulo', apiId: null, logo: '/Simulador-Libertadores/logos/337.png' },
+  { id: 'atletico_nacional', name: 'Atlético Nacional', apiId: null, logo: '/Simulador-Libertadores/logos/427.png' },
+  { id: 'ldu_quito', name: 'LDU Quito', apiId: null, logo: '/Simulador-Libertadores/logos/5270338.png' },
+  { id: 'botafogo', name: 'Botafogo', apiId: null, logo: '/Simulador-Libertadores/logos/316.png' },
+  { id: 'river_plate', name: 'River Plate', apiId: null, logo: '/Simulador-Libertadores/logos/94.png' },
+  { id: 'libertad', name: 'Libertad', apiId: null, logo: '/Simulador-Libertadores/logos/5280161.png' },
+  { id: 'palmeiras', name: 'Palmeiras', apiId: null, logo: '/Simulador-Libertadores/logos/329.png' },
+  { id: 'universitario', name: 'Universitario', apiId: null, logo: '/Simulador-Libertadores/logos/1450.png' },
   
   // Right side teams
-  { id: 'velez_sarsfield', name: 'Vélez Sarsfield', apiId: null, logo: '/logos/98.png' },
-  { id: 'fortaleza', name: 'Fortaleza', apiId: null, logo: '/logos/104750.png' },
-  { id: 'racing_club', name: 'Racing Club', apiId: null, logo: '/logos/93.png' },
-  { id: 'penarol', name: 'Peñarol', apiId: null, logo: '/logos/1922.png' },
-  { id: 'estudiantes', name: 'Estudiantes', apiId: null, logo: '/logos/85.png' },
-  { id: 'cerro_porteno', name: 'Cerro Porteño', apiId: null, logo: '/logos/1445.png' },
-  { id: 'internacional', name: 'Internacional', apiId: null, logo: '/logos/326.png' },
-  { id: 'flamengo', name: 'Flamengo', apiId: null, logo: '/logos/322.png' },
+  { id: 'velez_sarsfield', name: 'Vélez Sarsfield', apiId: null, logo: '/Simulador-Libertadores/logos/98.png' },
+  { id: 'fortaleza', name: 'Fortaleza', apiId: null, logo: '/Simulador-Libertadores/logos/104750.png' },
+  { id: 'racing_club', name: 'Racing Club', apiId: null, logo: '/Simulador-Libertadores/logos/93.png' },
+  { id: 'penarol', name: 'Peñarol', apiId: null, logo: '/Simulador-Libertadores/logos/1922.png' },
+  { id: 'estudiantes', name: 'Estudiantes', apiId: null, logo: '/Simulador-Libertadores/logos/85.png' },
+  { id: 'cerro_porteno', name: 'Cerro Porteño', apiId: null, logo: '/Simulador-Libertadores/logos/1445.png' },
+  { id: 'internacional', name: 'Internacional', apiId: null, logo: '/Simulador-Libertadores/logos/326.png' },
+  { id: 'flamengo', name: 'Flamengo', apiId: null, logo: '/Simulador-Libertadores/logos/322.png' },
 ];
