f0676c69ec8fefea9fd1a9fdafca2d7f03c0b25d
llorenzoaugusto <114509802+llorenzoaugusto@users.noreply.github.com>
2025-06-03 04:15:10 +0000

    fix: ajusta configuração do build para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-export default defineConfig(() => {
-    return {
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, './src'),
-        }
-      },
-      base: '/Simulador-Libertadores/'
-    };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    }
+  },
+  base: '/Simulador-Libertadores/',
+  server: {
+    port: 5173,
+    strictPort: true,
+  }
 });
